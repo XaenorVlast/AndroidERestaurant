2d0979837094c2d9999d66acc585ca7a4c8c2a5d
Loick <loick.gomez@isen.yncrea.fr>
2024-03-22 12:31:22 +0100

    fin des styles

--- app/src/main/java/fr/isen/gomez/androiderestaurant/HomeActivity.kt
+++ app/src/main/java/fr/isen/gomez/androiderestaurant/HomeActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.MaterialTheme
@@ -20,8 +21,16 @@ import fr.isen.gomez.androiderestaurant.ui.theme.AndroidERestaurantTheme
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Icon
 import androidx.compose.ui.Modifier
-
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.Alignment
 
 
 class HomeActivity : ComponentActivity() {
@@ -46,29 +55,42 @@ class HomeActivity : ComponentActivity() {
 
 @Composable
 fun MenuScreen(context: Context) {
-    Column {
-        val categories = listOf("Entrées", "Plats", "Desserts")
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize().padding(16.dp)
+    ) {
         Text(
-            "Bienvenue chez DroidRestaurant",
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp),
+            "Bienvenue chez\nDroidRestaurant",
             textAlign = TextAlign.Center,
-            fontSize = 24.sp
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color(0xFFF57C00), // Couleur orange personnalisée
+            modifier = Modifier.padding(bottom = 32.dp)
         )
-        // Affichage des catégories
+
+        // Inclure l'icône ici
+        Image(
+            painter = painterResource(id = R.drawable.androidcook),
+            contentDescription = "Icône du Chef",
+            modifier = Modifier.size(100.dp) // Modifiez ceci selon la taille souhaitée
+        )
+
+        // Affichage des catégories avec séparateurs
+        val categories = listOf("Entrées", "Plats", "Desserts")
         categories.forEach { category ->
             CategoryItem(name = category, onClick = { categoryName ->
-                // Intention pour démarrer fr.isen.gomez.androiderestaurant.CategoryActivity avec le nom de la catégorie
                 val intent = Intent(context, CategoryActivity::class.java).apply {
                     putExtra("categoryName", categoryName)
                 }
                 context.startActivity(intent)
             })
+            Divider(
+                color = Color(0xFFF57C00), // Couleur orange personnalisée
+                modifier = Modifier.padding(vertical = 8.dp)
+            )
         }
     }
 }
-
 
 @Composable
 fun CategoryItem(name: String, onClick: (String) -> Unit) {
@@ -79,6 +101,9 @@ fun CategoryItem(name: String, onClick: (String) -> Unit) {
             .clickable { onClick(name) }
             .padding(16.dp),
         textAlign = TextAlign.Center,
-        fontSize = 18.sp
+        fontSize = 18.sp,
+        fontWeight = FontWeight.Medium,
+        color = Color.Black
     )
 }
+
